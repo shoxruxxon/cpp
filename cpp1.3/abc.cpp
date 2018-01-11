08032e7bbda9f162d4d48873d38d889ecc9dcbbe
shoxruxxon <32229625+shoxruxxon@users.noreply.github.com>
2018-01-11 22:22:39 +0300

    Create abc.cpp

---
+++ cpp1.3/abc.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("\n%d+%d+%d=%d", a, b, c, a + b + c);
+}
